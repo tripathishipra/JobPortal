5c09c6e63bccf4f208d8cea9706722ea94ef4c94
Shipra Tripathi <104627391+tripathishipra@users.noreply.github.com>
2024-12-14 23:25:27 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+    css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
